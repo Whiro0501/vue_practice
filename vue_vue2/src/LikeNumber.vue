a4e209b2c3c91c1c935645163fe855b060f27421
Hiroki Watanabe <x5dwimpej@yahoo.co.jp>
2020-09-10 21:31:04 +0900

    component作成

---
+++ vue_vue2/src/LikeNumber.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <p>いいね{{ number }}</p>
+  <button @click="countUp">button</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 5,
+    };
+  },
+    methods:  {
+      countUp(){
+        this.number += 1;
+      }
+    },
+};
+</script>
