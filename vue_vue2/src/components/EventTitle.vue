504d41bed800fb789974038e408fd7c6cdbbd5ea
Hiroki Watanabe <x5dwimpej@yahoo.co.jp>
2020-09-13 00:02:08 +0900

    カスタムディレクティブ

--- vue_vue2/src/components/EventTitle.vue
+++ vue_vue2/src/components/EventTitle.vue
@@ -16,6 +16,6 @@
 
 <script>
 export default {
-  props: ["value"]
-}
+    props: ['value'],
+};
 </script>
--- vue_vue2/src/main.js
+++ vue_vue2/src/main.js
@@ -4,9 +4,16 @@ import LikeNumber from './components/LikeNumber'
 
 Vue.config.productionTip = false
 Vue.component("LikeNumber", LikeNumber)
+Vue.directive("border", {
+    bind(el, bindingm vnode) {},
+    inserted() {},
+    update(el, bindingm vnode) {},
+    componentUpdated() {},
+    unbind() {},
+});
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
 
 console.log(App);
