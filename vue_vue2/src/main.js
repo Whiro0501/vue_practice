fb8e01ae2d3acb01037e7e86e944a524ecd929d3
Hiroki Watanabe <x5dwimpej@yahoo.co.jp>
2020-09-13 00:14:20 +0900

    カスタムディレクティブ2

--- vue_vue2/src/main.js
+++ vue_vue2/src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import LikeNumber from './components/LikeNumber'
 
-Vue.config.productionTip = false
-Vue.component("LikeNumber", LikeNumber)
-Vue.directive("border", {
-    bind(el, bindingm vnode) {},
-    inserted() {},
-    update(el, bindingm vnode) {},
-    componentUpdated() {},
-    unbind() {},
+Vue.config.productionTip = false;
+Vue.component("LikeNumber", LikeNumber);
+Vue.directive("border", function (el,binding) {
+    el.style.border = "solid black 2px";
+    el.style.borderWidth = binding.value
 });
 
 new Vue({
