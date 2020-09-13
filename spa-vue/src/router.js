93b38657585f6b9f2dc515d7471917664f09f880
Hiroki Watanabe <x5dwimpej@yahoo.co.jp>
2020-09-13 23:37:15 +0900

    router-詳細画面

--- spa-vue/src/router.js
+++ spa-vue/src/router.js
@@ -8,5 +8,5 @@ Vue.use(Router);
 export default new Router({
     mode: "history",
     routes: [{ path: "/", component: Home },
-    { path: "/users", component: Users }]
+    { path: "/users/:id", component: Users }]
 });
--- spa-vue/src/views/Users.vue
+++ spa-vue/src/views/Users.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
         <h3>Users</h3>
+        <router-link to="/users/1" >ユーザ１</router-link>
+        <router-link to="/users/2">ユーザ2</router-link>
+        <hr>
+        <h1> User No. {{ $route.params.id }}</h1>
     </div>
 </template>
+
+
+<script>
+export default {
+    watch: {
+        $route: function(to, from){
+            console.log(to);
+            console.log(from);
+            console.log('route!!!');
+        }
+    },
+}
+</script>
