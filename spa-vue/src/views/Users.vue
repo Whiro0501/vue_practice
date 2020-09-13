b18d2e2ff98cd1a60c54b23403ab7f6dea0e6649
Hiroki Watanabe <x5dwimpej@yahoo.co.jp>
2020-09-14 00:18:33 +0900

    コンポーネントを疎結合にする

--- spa-vue/src/views/Users.vue
+++ spa-vue/src/views/Users.vue
@@ -4,13 +4,14 @@
         <router-link to="/users/1" >ユーザ１</router-link>
         <router-link to="/users/2">ユーザ2</router-link>
         <hr>
-        <h1> User No. {{ $route.params.id }}</h1>
+        <h1> User No. {{ id }}</h1>
     </div>
 </template>
 
 
 <script>
 export default {
+    props: ["id"],
     watch: {
         $route: function(to, from){
             console.log(to);
