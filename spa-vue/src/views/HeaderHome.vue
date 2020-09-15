2ab5cc73080906232d7bc3ed2c0ccd2d61f91fbf
Hiroki Watanabe <x5dwimpej@yahoo.co.jp>
2020-09-15 22:21:20 +0900

    mapActions

--- spa-vue/src/views/HeaderHome.vue
+++ spa-vue/src/views/HeaderHome.vue
@@ -4,22 +4,18 @@
             <h2>Home</h2>
             <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
             <router-link to="/users" class="link" active-class="link--active" exact>Users</router-link>
-            <button @click="increment">+1</button>
-            <button @click="decrement">-1</button>
+            <button @click="increment(2)">+1</button>
+            <button @click="decrement(2)">-1</button>
         </nav>
     </div>
 </template>
 
 <script>
-
+import {mapActions} from 'vuex';
 export default {
     methods: {
-        increment(){
-            this.$store.dispatch('increment', 2);
-        },
-        decrement(){
-            this.$store.dispatch('decrement', 2);
-        }
+        ...mapActions(["increment", "decrement"])
+        
     }
 };
 </script>
